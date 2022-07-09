e80af9e6c5ab291823820d85a8e33f9ce5f792ed
shijinbao1996 <1366221851@qq.com>
2022-07-09 20:28:04 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="footer_wrap">
+      <!-- <a href="#/find">发现音乐</a>
+      <a href="#/my">我的音乐</a>
+      <a href="#/part?username=jerry">朋友</a> -->
+
+      <!-- <router-link to="/find">发现音乐</router-link>
+      <router-link to="/my">我的音乐</router-link>
+      <router-link to="/part/:username=jerry/:id=1">朋友</router-link> -->
+
+      <span @click="btn('/find', 'Find')">发现音乐</span>
+      <span @click="btn('/my', 'My')">我的音乐</span>
+      <span @click="btn('/part', 'Part')">朋友</span>
+    </div>
+    <div class="top">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  methods: {
+    btn(targetPath, targetName) {
+      // 方式1: path跳转
+      this.$router.push({
+        // path: targetPath,
+        name: targetName,
+        query: {
+          username: "jerry",
+          id: 1,
+        },
+      });
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.footer_wrap {
+  position: fixed;
+  left: 0;
+  top: 0;
+  display: flex;
+  width: 100%;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #333;
+  color: #ccc;
+}
+.footer_wrap span {
+  flex: 1;
+  text-decoration: none;
+  padding: 20px 0;
+  line-height: 20px;
+  background-color: #333;
+  color: #ccc;
+  border: 1px solid black;
+}
+.footer_wrap span:hover {
+  background-color: #555;
+}
+.top {
+  padding-top: 62px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,86 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Find from '@/views/Find'
+import My from '@/views/My'
+import Part from '@/views/Part'
+import NotFound from '@/views/NotFound'
+import Ranking from '@/views/Second/Ranking'
+import Recommend from '@/views/Second/Recommend'
+import SongList from '@/views/Second/SongList'
 
+
+let routerPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return routerPush.call(this, location).catch(err => err)
+}
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+
+// 建立路由规则
+const routes = [
+  {
+    path:'/',
+    redirect: '/find'
+  },
+  {
+    path:'/find',
+    component:Find,
+    name:'Find',
+    children:[
+      {
+        path:'ranking',
+        component:Ranking,
+      },
+      {
+        path:'recommend',
+        component:Recommend,
+      },
+      {
+        path:'songlist',
+        component:SongList,
+      },
+    ]
+  },
+  {
+  path:'/my',
+  component:My ,
+  name:'My'
+  },
+  {
+    path:'/part/:username/:id',
+    component:Part  ,
+    name:'Part'
+    },
+    {
+      path:'*',
+      component:NotFound
+    }
+]
+
+// 创建路由对象：传入规则
+const router = new VueRouter({
+  routes,
+  // mode:history
+})
+
+const isLogin = false
+router.beforeEach((to,from,next)=>{
+  // console.log(to);
+   if(to.path == '/my' && !isLogin) {
+    alert('login')
+    next(false)
+   } else{
+    next()
+   }
+   
+})
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
+  
 }).$mount('#app')
