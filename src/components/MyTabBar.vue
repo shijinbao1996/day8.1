f2100dea59dafbef5f8b36c8c84c8ffe38ad7db7
shijinbao1996 <1366221851@qq.com>
2022-07-09 23:29:04 +0800

    first commit

---
+++ src/components/MyTabBar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="my-tab-bar">
+    <div
+      class="tab-item"
+      :class="{ current: item.componentName == currentIndex }"
+      v-for="item in list"
+      :key="item.componentName"
+      @click="clickFn(item.componentName)"
+    >
+      <span class="iconfont" :class="item.iconText"></span>
+      <span>{{ item.text }}</span> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentIndex: "MyGoodsList",
+      currentUrl: "/list",
+    };
+  },
+  methods: {
+    clickFn(val) {
+      this.currentIndex = val;
+      console.log(val);
+      this.$emit("changeTab", val);
+      this.$router.push({
+        // path: targetPath,
+        name: val,
+      });
+    },
+  },
+  props: {
+    list: {
+      type: Array,
+      validator(val) {
+        if (val.length >= 2 && val.length <= 5) {
+          return true;
+        }
+        return false;
+      },
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.my-tab-bar {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  border-top: 1px solid #ccc;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: white;
+  .tab-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+
+.current {
+  color: #1d7bff;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,51 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import "bootstrap/dist/css/bootstrap.css"
+import "./assets/fonts/iconfont.css"
+
+import MyGoodsList from '@/views/MyGoodsList'
+import MyGoodsSearch from '@/views/MyGoodsSearch'
+import MyUserInfo from '@/views/MyUserInfo'
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+
+const routes = [
+  {
+    path: "/list",
+    component: MyGoodsList,
+    name:'MyGoodsList'
+  },
+  {
+    path: "/search",
+    component: MyGoodsSearch,
+    name:'MyGoodsSearch'
+  },
+  {
+    path: "/info",
+    component: MyUserInfo,
+    name:'MyUserInfo'
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+import axios from 'axios';
+axios.defaults.baseURL = 'https://www.escook.cn';
+Vue.prototype.$axios =axios;
+
+Vue.directive('focus',{
+  inserted(el){
+    el.focus()
+  }
+})
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
